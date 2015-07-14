454c9bd52545da68b6884cc7b910e080bcef5ab4
yestuartgithub <yestuart@utexas.edu>
2015-07-14 10:07:14 -0500

    Let them eat cake: written in RStudio

---
+++ LetThemEatCake.R
@@ -0,0 +1,7 @@
+LouisXVI <- rnorm(1793, 180, 5)
+head(LouisXVI)
+hist(LouisXVI)
+off.with.his.head <- which(LouisXVI<175)
+guillotine <- LouisXVI[off.with.his.head]
+max(guillotine) 
+hist(guillotine) #Top gone from that distribution. Macabre?
